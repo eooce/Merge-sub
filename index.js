a727f42fb7075aefe49eb67cfd2b0c67bbcaf3e1
Okka <142894633+eoovve@users.noreply.github.com>
2023-11-07 16:43:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 7860;
 
 // 订阅链接数组
 const subscriptions = [
-    'https://www.bing.com/sub',         // 此处备注名称
-    'https://www.sina.com/sub',         // 此处备注名称
-    'https://www.yahoo.com/sub',        // 此处备注名称
-    'https://www.sougou.com/sub',       // 此处备注名称
-     // 继续添加更多订阅链接
+  'https://www.bing.com/sub',         // 此处备注名称
+  'https://www.sina.com/sub',         // 此处备注名称
+  'https://www.yahoo.com/sub',        // 此处备注名称
+  'https://www.sougou.com/sub',       // 此处备注名称
+   // 继续添加更多订阅链接
 ];
 
-// 添加新的订阅链接
 function addSubscription(subscription) {
   subscriptions.push(subscription);
 }
 
-// 处理 POST 请求以添加新的订阅链接
 app.use(express.json());
 
 app.post('/add-subscription', (req, res) => {
@@ -25,18 +24,40 @@ app.post('/add-subscription', (req, res) => {
   res.status(200).send('Subscription added successfully');
 });
 
-// 生成总订阅链接
-function generateMergedSubscription() {
-  return subscriptions.join('\n');
+function decodeBase64Content(base64Content) {
+  const decodedContent = Buffer.from(base64Content, 'base64').toString('utf-8');
+  return decodedContent;
 }
 
-// 添加总订阅链接的路由
-app.get('/su', (req, res) => {
-  const mergedSubscription = generateMergedSubscription();
+async function fetchSubscriptionContent(subscription) {
+  try {
+    const response = await axios.get(subscription);
+    return response.data;
+  } catch (error) {
+    console.error(`Error fetching subscription content: ${error}`);
+    return null;
+  }
+}
+
+async function generateMergedSubscription() {
+  const mergedContent = [];
+  for (const subscription of subscriptions) {
+    const subscriptionContent = await fetchSubscriptionContent(subscription);
+    if (subscriptionContent) {
+      const decodedContent = decodeBase64Content(subscriptionContent);
+      mergedContent.push(decodedContent);
+    }
+  }
+  return mergedContent.join('\n');
+}
+
+
+app.get('/sum', async (req, res) => {
+  const mergedSubscription = await generateMergedSubscription();
   res.setHeader('Content-Type', 'application/octet-stream');
   res.send(mergedSubscription);
 });
-//静态伪装网页
+
 app.use(express.static(__dirname));
 
 app.listen(port, () => {
