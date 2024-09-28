89eebafc95e33278fe750686bba364932b077937
eooce <142894633+eooce@users.noreply.github.com>
2024-09-29 01:16:27 +0800

    worker订阅合并，订阅链接+自建节点

--- workers/worker_sub.js
+++ workers/worker_sub.js
@@ -1,21 +1,36 @@
-// 订阅：workers域名/sub  sub路径可自定义
-// 可带参数：workers域名/sub?CFIP=优选ip&CFPORT=优选ip端口
-// 例如：https://test.abc.workers.dev/sub?CFIP=47.75.222.188&CFPORT=7890
+// 订阅：workers域名/sub  sub路径可定义
+// 带参数订阅：workers域名/sub?CFIP=优选ip&CFPORT=优选ip端口
+// 例如：https://test.abc.worker.dev/sub?CFIP=47.75.222.188&CFPORT=7890
 
 let CFIP = "www.visa.com.tw";  // 优选ip或优选域名
 let CFPORT = "443";            // 优选ip或有序域名对应的端口
 const SUB_PATH = '/sub';       // 访问路径
 
+// 添加多个订阅链接，以下示列节点订阅添加前请删除
+const subscriptions = [
+  'https://www.google/sub',                          
+  'https://www.google/sub',
+  'https://www.google/sub',  
+  'https://www.google/sub'  // 最后一个没有逗号
+
+  // ... 添加更多订阅链接
+];
+
+// 支持添加单条或多条自建节点，可以为空,以下示列节点添加前请删除
+const nodes = `
+vless://9afd1229-b893-40c1-84dd-51e7ce204913@www.visa.com.tw:8443?encryptio  
+vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIkdsaXRjaC1VUy1BbWF6b24uY29tIiwNCiA
+trojan://9afd1229-b893-40c1-84dd-51e7ce204913@www.visa.com.tw:8443?security
+hysteria2://6991e58f-a9af-4319-a8e8-e9c4df14d650@156.255.90.161:17397/?sni
+tuic://6991e58f-a9af-4319-a8e8-e9c4df14d650:YDU08bdN1no66Eufx7XdGbVJ@156.25
+
+`;
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
 
-const subscriptions = [
-  'https://www.google.com/sub',                          
-  'https://www.google.com/sub',
-  'https://www.google.com/sub'
-  // ... 添加更多子订阅链接
-];
+const nodeArray = nodes.trim().split('\n').filter(node => node); 
 
 async function handleRequest(request) {
   const url = new URL(request.url);
@@ -40,6 +55,10 @@ async function handleRequest(request) {
       const newSubscription = await request.json();
       addSubscription(newSubscription.subscription);
       return new Response('Subscription added successfully', { status: 200 });
+  } else if (url.pathname === '/add-nodes' && request.method === 'POST') {  
+      const newNodes = await request.json();
+      addMultipleNodes(newNodes.nodes); 
+      return new Response('Nodes added successfully', { status: 200 });
   }
 
   return new Response('Hello world!', { status: 200 });
@@ -49,7 +68,14 @@ function addSubscription(subscription) {
   subscriptions.push(subscription);
 }
 
+function addMultipleNodes(nodes) {
+  subscriptions.push(...nodes); 
+}
+
 async function fetchSubscriptionContent(subscription) {
+  if (!subscription.startsWith('http://') && !subscription.startsWith('https://')) {
+    return null; 
+  }
   const response = await fetch(subscription);
   return response.ok ? response.text() : null;
 }
@@ -68,24 +94,21 @@ function replaceAddressAndPort(content) {
           const base64Part = line.substring(8);
           const decodedVmess = decodeBase64Content(base64Part);
           const vmessObj = JSON.parse(decodedVmess);
-
           vmessObj.add = CFIP;
           vmessObj.port = parseInt(CFPORT, 10);
-
           const updatedVmess = btoa(JSON.stringify(vmessObj));
           return `vmess://${updatedVmess}`;
       } else if (line.startsWith('vless://') || line.startsWith('trojan://')) {
-          // 使用正则表达式匹配并替换 host 和 port
           return line.replace(/@([\w.-]+):(\d+)/, (match, host) => {
               return `@${CFIP}:${CFPORT}`;
           });
       }
-
       return line;
   }).join('\n');
 }
 
 async function generateMergedSubscription() {
+  const nodesContent = nodeArray.join('\n'); 
   const promises = subscriptions.map(async (subscription) => {
       const subscriptionContent = await fetchSubscriptionContent(subscription);
       if (subscriptionContent) {
@@ -97,5 +120,6 @@ async function generateMergedSubscription() {
   });
 
   const mergedContentArray = await Promise.all(promises);
+  mergedContentArray.unshift(nodesContent);
   return mergedContentArray.filter(content => content !== null).join('\n');
 }
